5de161081e08e94bd90c9bf07b6c12d5396ff2fe
Douglas Lopez <lopez@pythian.com>
2019-11-28 16:11:06 -0600

    Added autoscaling group bootstraping

---
+++ source/infrastructure-as-code/iac-terraform4-alb/asg.tf
@@ -0,0 +1,47 @@
+data "aws_ami" "amazon_linux_ec2" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+}
+
+data "template_file" "user_data" {
+  template = "${file("${path.module}/template/user-data.tpl")}"
+}
+
+data "aws_availability_zones" "all" {}
+
+resource "aws_launch_configuration" "terraformlab4" {
+  image_id = "${data.aws_ami.amazon_linux_ec2.id}"
+  instance_type = "${var.aws_instance_type}"
+  key_name = "demo"
+  security_groups = [ "${aws_security_group.security_group.id}" ]
+  user_data = "${data.template_file.user_data.rendered}"
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+resource "aws_autoscaling_group" "terraform-ASGlab4" {
+  launch_configuration = "${aws_launch_configuration.terraformlab4.id}"
+  vpc_zone_identifier       = ["subnet-02e2501d089873c29", "subnet-0d45a460e80029489"]
+
+  load_balancers = ["${aws_elb.terraformlab4.name}"]
+  health_check_type = "EC2"
+  max_size = 10
+  min_size = 2
+  tag {
+    Name =  "${var.name}"
+    propagate_at_launch = true
+  }
+}
---
+++ source/infrastructure-as-code/iac-terraform4-alb/elb.tf
@@ -0,0 +1,49 @@
+resource "aws_elb" "terraformlab4" {
+  name               = "${var.name}"
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 8000
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port      = 8080
+    instance_protocol  = "http"
+    lb_port            = 8080
+    lb_protocol        = "http"
+  }
+
+  listener {
+    instance_port      = 8081
+    instance_protocol  = "http"
+    lb_port            = 8081
+    lb_protocol        = "http"
+  }
+
+  listener {
+    instance_port      = 8082
+    instance_protocol  = "http"
+    lb_port            = 8082
+    lb_protocol        = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  subnets = ["subnet-049706ae911f72263","subnet-08bb614d5f1235d54"]
+
+  tags = {
+    Name =  "${var.name}"
+  }
+}
